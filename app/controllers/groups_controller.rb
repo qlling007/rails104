e8a5eb25f317f60291c7b4a8926a5f9bd7554525
qinglingling <qlingling007@gmail.com>
2017-03-10 21:12:28 +0800

    move form to partial

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -26,7 +26,7 @@ class GroupsController < ApplicationController
   def update
     @group = Group.find(params[:id])
     if @group.update(group_params)
-    redirect_to groups_path, notice: "Update Success"
+      redirect_to groups_path, notice: "Update Success"
     else
       render :edit
     end
