b722d5e6470cb3860c097b8b12821fc5d3587231
qinglingling <qlingling007@gmail.com>
2017-03-11 10:37:05 +0800

    add permission check on show page

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -36,7 +36,7 @@ class GroupsController < ApplicationController
     end
   end
   def destroy
-  
+
     @group.destroy
     flash[:alert] = "Group deleted"
     redirect_to groups_path
@@ -46,7 +46,7 @@ class GroupsController < ApplicationController
   def find_group_and_check_permission
     @group = Group.find(params[:id])
     if current_user != @group.user
-      redirect_to root_path, alert: "you have no permition"
+      redirect_to root_path, alert: "you have no permission"
 
     end
 
